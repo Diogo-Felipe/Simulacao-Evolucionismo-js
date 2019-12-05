abc4ae3f8bfe99f38b956f259c3a4f52e9a58de8
Diogo Felipe <dlcfelipp@gmail.com>
2019-12-05 19:29:29 -0300

    Adicinada documentação do proheto

--- Comida.js
+++ Comida.js
@@ -1,16 +1,37 @@
-
+/** 
+ * Classe que representa uma comida na simulacao 
+ * @author Diogo L.C. Felipe <dlcfelipp@gmail.com>
+ */
 class Comida {
+    /**
+     * Criacao de uma comida
+     * @param {number} posicaoX - Posicao da comida no eixo x
+     * @param {number} posicaoY - Posicao da comida no eico y
+     */
     constructor(posicaoX, posicaoY) {
+
+        /** @private */
         this.disponivel = true;
         
+        /** @private */
         this.posicaoX = posicaoX;
+
+        /** @private */
         this.posicaoY = posicaoY;
     }
 
+    /**
+     * Verifica se a comida ja foi consumida ou nao
+     * @returns {boolean} Disponibilidade da comida 
+     */
     estahDisponivel(){
         return this.disponivel;
     }
 
+    /**
+     * Pega a posicao cartesiana da comida
+     * @returns {number[]} .x e .y da comida
+     */
     getPosicao(){
         return {
             x: this.posicaoX,
@@ -18,10 +39,16 @@ class Comida {
         };
     }
 
+    /**
+     * Consome a comida, deixando-a indisponivel
+     */
     consumir(){
         this.disponivel = false;
     }
 
+    /**
+     * Restaura comida, colocando-a em uma nova posicao
+     */
     reseta(){
         this.disponivel = true;
         this.posicaoX = Math.floor(Math.random() * 100);
--- main.js
+++ main.js
@@ -1,15 +1,30 @@
+/**
+ * @file Local ideal para realizar as simulacoes. Contem um exemplo de simulacao
+ * @author Diogo L.C. Felipe <dlcfelipp@gmail.com>
+ */
+
+/** @type {number[]} */
 var listaIndividuos = [];
+/** @type {number[]}*/
 var listaComida = [];
 
+/** @const {number} */
 const energiaBase = 500;
+/** @const {number} */
 const numeroDeDiasSimulados = 1000;
+/** @const {number} */
 const qtdInicialDeIndividuos = 20;
+/** @const {number} */
 const qtdDeComida = 100;
 
+/** @type {Object} */
 let processadorDeSaida = new ProcessadorDeSaida();
 
+/** @type {Object} */
 let simulador = new Simulacao(numeroDeDiasSimulados, energiaBase, qtdInicialDeIndividuos, qtdDeComida, listaIndividuos, listaComida, processadorDeSaida);
 
+/** Inicia a simulacao */
 simulador.simula(numeroDeDiasSimulados, energiaBase, qtdInicialDeIndividuos, qtdDeComida)
 
+/** Gera arquivo com a saida de dados do simulador */
 processadorDeSaida.geraArquivoDeDados(simulador.getMediaPorGeracao());
